a9a8668a86535a59b07bce422a5f06b6043ca486
Alec L. Robitaille <robit.alec@gmail.com>
2024-01-23 16:50:01 -0400

    drop lower temp fix

--- R/count_list.R
+++ R/count_list.R
@@ -1,5 +1,5 @@
 count_list <- function(DT, col) {
   count <- DT[, strsplit(.SD[[1]], ';'), .SDcols = col, by = covidence_number]
-  count[, V1 := tolower(trimws(V1))]
+  count[, V1 := trimws(V1)]
   return(count)
 }
