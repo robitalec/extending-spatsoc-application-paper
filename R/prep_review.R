2b0ed190992dad7b2e359d733f7766d16b0ec2c1
Alec L. Robitaille <robit.alec@gmail.com>
2024-02-05 09:36:55 -0400

    fix metric agg

--- R/prep_review.R
+++ R/prep_review.R
@@ -2,13 +2,11 @@ prep_review <- function(DT, metric_synonyms = NULL) {
   setnames(DT, make_clean_names(colnames(DT)))
 
   if (!is.null(metric_synonyms)) {
-    DT[, metric_agg :=
-         stringi::stri_replace_all(
-           str = metric_used_or_described,
-           replacement = metric_synonyms$metric_agg,
-           regex = metric_synonyms$metric,
-           vectorize_all = FALSE
-         )]
+    DT[, metric_agg := metric_synonyms[
+      metric %in% trimws(unlist(strsplit(.BY[[1]], ';'))),
+      paste(metric_agg, collapse = '; ')],
+      by = metric_used_or_described
+    ]
   }
 
   return(DT)
