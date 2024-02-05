57c1440e99e4ddb3f6de8d40b05bdf1b97d590dd
Alec L. Robitaille <robit.alec@gmail.com>
2024-02-05 08:52:55 -0400

    add metric_synonyms arg, str replace all

--- R/prep_review.R
+++ R/prep_review.R
@@ -1,7 +1,15 @@
-prep_review <- function(path) {
-  DT <- fread(path)
-
+prep_review <- function(DT, metric_synonyms = NULL) {
   setnames(DT, make_clean_names(colnames(DT)))
+
+  if (!is.null(metric_synonyms)) {
+    DT[, metric_agg :=
+         stringi::stri_replace_all(
+           str = metric_used_or_described,
+           replacement = metric_synonyms$metric_agg,
+           regex = metric_synonyms$metric,
+           vectorize_all = FALSE
+         )]
+  }
 
   return(DT)
 }
