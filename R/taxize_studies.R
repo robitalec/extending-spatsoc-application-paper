11426d567109a4f1c1f99aa948e882d57b7cc107
Alec L. Robitaille <robit.alec@gmail.com>
2024-02-06 00:33:53 -0400

    parse, fields info

--- R/taxize_studies.R
+++ R/taxize_studies.R
@@ -2,3 +2,16 @@ taxize_studies <- function(DT) {
   DT_in <- DT[, strsplit(.SD[['species']], ';'), by = covidence_number]
   DT_in[, species := trimws(V1)]
   DT_in[, V1 := NULL]
+
+  # Field 	Meaning
+  # Id 	UUID v5 generated out of Verbatim
+  # Verbatim 	Input name-string without any changes
+  # Cardinality 	0 - N/A, 1 - Uninomial, 2 - Binomial etc.
+  # CanonicalStem 	Simplest canonical form with removed suffixes
+  # CanonicalSimple 	Simplest canonical form
+  # CanonicalFull 	Canonical form with hybrid sign and ranks
+  # Authors 	Authorship of a name
+  # Year 	Year of the name (if given)
+  # Quality 	Parsing quality
+  DT_parse <- gn_parse_tidy(DT_in[, unique(species)])
+  setDT(DT_parse)
