365dd57ab22585a05087bfaa79a734c9a796a8fe
Alec L. Robitaille <robit.alec@gmail.com>
2024-02-05 23:34:57 -0400

    split on ; and trimws

--- R/geocode_studies.R
+++ R/geocode_studies.R
@@ -1,4 +1,6 @@
 geocode_studies <- function(DT) {
-  DT[, region := trimws(region)]
+  DT_in <- DT[, strsplit(.SD[['region']], ';'), by = covidence_number]
+  DT_in[, region := trimws(V1)]
+  DT_in[, V1 := NULL]
 
 
