c45685bfaccfc31e95300909b210863f3c63ea51
Alec L. Robitaille <robit.alec@gmail.com>
2024-06-23 20:35:12 -0300

    tidy

--- R/draft/calc_dir_corr_delay.R
+++ R/draft/calc_dir_corr_delay.R
@@ -1,44 +1,4 @@
-calc_dir_corr_delay <- function(DT, window = 5) {
-  calc_az(DT)
-  dyads <- CJ(
-    ID1 = unique(DT$id),
-    ID2 = unique(DT$id),
-    tg = unique(DT$timegroup)
-  )[ID1 != ID2]
-  setorder(DT, -timegroup)
-  setorder(dyads, tg)
-
-  dyads[, calc_closest_az(.SD, DT, window),
-        by = tg,
-        .SDcols = colnames(dyads)]
-}
-
-calc_closest_az <- function(dyad_DT, locs_DT, window) {
-  i_tg <- unique(dyad_DT$tg)
-  seq_tg <- seq(i_tg - window, i_tg + window) - i_tg
-  dyad_DT[, #.(tg_delay =
-          seq_tg[which.min(
-          abs_diff_rad(
-    locs_DT[.SD, az, on = .(id == ID1, timegroup == tg)],
-    locs_DT[id == ID2 & between(timegroup, tg - window, tg + window), az]
-    )
-    )]
-    ,
-  # )) - window), # rm 1l
-  by = .(ID1, ID2), .SDcols = colnames(dyad_DT)]
-}
-
-
-cast_az <- function(DT) {
-  as.matrix(dcast(DT, timegroup ~ id, value.var = 'az')[,-c(1)])
-}
-
-
-
-
-
-
-calc_delay <- function(DT, window) {
+calc_dir_corr_delay <- function(DT, window) {
   setorder(DT, timegroup)
   id_tg <- DT[, .(ID1 = unique(id), focal_az = az),
               by = .(tg = timegroup)][!is.na(focal_az)]
@@ -48,6 +8,3 @@ calc_delay <- function(DT, window) {
        by = .(ID2 = id)]
   }, by = .(ID1, tg)]
 }
-calc_az(DT_test)
-calc_delay(DT_test, window = 3)
-DT_test
