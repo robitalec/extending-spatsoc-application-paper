ae9a73835d4bc3d1fbbc83614fac62017227c2e3
Alec L. Robitaille <robit.alec@gmail.com>
2024-06-23 17:12:15 -0300

    more trying

--- R/draft/calc_dir_corr_delay.R
+++ R/draft/calc_dir_corr_delay.R
@@ -5,15 +5,30 @@ calc_dir_corr_delay <- function(DT, window = 5) {
     ID2 = unique(DT$id),
     tg = unique(DT$timegroup)
   )[ID1 != ID2]
+  setorder(DT, -timegroup)
+  setorder(dyads, tg)
 
-  calc_closest_az <- function(dyad_DT, locs_DT, window) {
-    dyad_DT[, .(tg_delay = which.min(abs_diff_rad(
-      locs_DT[.SD, az, on = .(id == ID1, timegroup == tg)],
-      locs_DT[id == ID2 & between(timegroup, tg - window, tg + window), az]
-    )) - window - 1L),
-    by = .(ID1, ID2), .SDcols = colnames(dyad_DT)]
-  }
   dyads[, calc_closest_az(.SD, DT, window),
         by = tg,
         .SDcols = colnames(dyads)]
+}
+
+calc_closest_az <- function(dyad_DT, locs_DT, window) {
+  i_tg <- unique(dyad_DT$tg)
+  seq_tg <- seq(i_tg - window, i_tg + window) - i_tg
+  dyad_DT[, #.(tg_delay =
+          seq_tg[which.min(
+          abs_diff_rad(
+    locs_DT[.SD, az, on = .(id == ID1, timegroup == tg)],
+    locs_DT[id == ID2 & between(timegroup, tg - window, tg + window), az]
+    )
+    )]
+    ,
+  # )) - window), # rm 1l
+  by = .(ID1, ID2), .SDcols = colnames(dyad_DT)]
+}
+
+
+cast_az <- function(DT) {
+  as.matrix(dcast(DT, timegroup ~ id, value.var = 'az')[,-c(1)])
 }
--- tests/test_directional_correlation_delay.R
+++ tests/test_directional_correlation_delay.R
@@ -12,35 +12,35 @@ targets::tar_source('R/draft')
 
 # Data --------------------------------------------------------------------
 DT_test_A <- data.table(
-  x = c(0, -10, 0, 10, 5),
-  y = c(0, 10, 20, 10, 5),
-  id =  'A'
+  x = c(0, 10),
+  y = c(0, 20),
+  timegroup = c(4, 5),
+  id =  'C'
 )
-DT_test_A[, datetime := seq.POSIXt(
-  as.POSIXct('2022-01-01 10:00:00'),
-  as.POSIXct('2022-01-02 10:00:00'),
-  length.out = .N),
-  by = id
-]
-
-group_times(DT_test_A, 'datetime')
-DT_test_A[, datetime := NULL]
 
 DT_test <- rbindlist(list(
   DT_test_A,
-  DT_test_A[, .(x, y, timegroup = shift(timegroup, 0, type = 'cyclic'), id = 'B')],
-  DT_test_A[, .(x, y, timegroup = shift(timegroup, -1, type = 'cyclic'), id = 'C')],
-  DT_test_A[, .(x, y, timegroup = shift(timegroup, 1, type = 'cyclic'), id = 'D')],
-  DT_test_A[, .(x, y, timegroup = shift(timegroup, 2, type = 'cyclic'), id = 'E')]
-), use.names = TRUE)
+  DT_test_A[, .(x, y, timegroup = timegroup - 2, id = 'A')],
+  DT_test_A[, .(x, y, timegroup = timegroup - 1, id = 'B')],
+  DT_test_A[, .(x, y, timegroup = timegroup + 1, id = 'D')]
+))
 
-setorder(DT_test, timegroup)
+setorder(DT_test, id)
 
 
 # Test --------------------------------------------------------------------
-calc_dir_corr_delay(DT_test, window  = 1)[ID1 == 'A']
+calc_dir_corr_delay(DT_test, window  = 1) |>
+  print()
 
+cast_az(DT_test)
 
+window <- 3
+DT_test[id == 'A' & !is.na(az)][,
+        apply(
+          dcast(DT_test[timegroup %in% seq.int(-window, window) + .BY[[2]]],
+                            timegroup ~ id, value.var = 'az')[, .SD, .SDcols = -c('timegroup')] - az,
+              MARGIN = 2, FUN = which.min),
+        by = .(id, timegroup)]
 
 # Plot --------------------------------------------------------------------
 g <- ggplot(DT_test) +
