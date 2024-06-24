62b3aca999a4fadf02b339bc23421737452f07ef
Alec L. Robitaille <robit.alec@gmail.com>
2024-06-24 11:50:59 -0300

    try passing edges

--- R/draft/calc_dir_corr_delay.R
+++ R/draft/calc_dir_corr_delay.R
@@ -1,12 +1,16 @@
 calc_dir_corr_delay <- function(DT, window) {
+calc_dir_corr_delay <- function(DT, edges, window) {
   setorder(DT, timegroup)
-  id_tg <- DT[, .(ID1 = unique(id), focal_az = az),
-              by = .(tg = timegroup)][!is.na(focal_az)]
+  setorder(edges, timegroup)
+  # id_tg <- DT[, .(ID1 = unique(id), focal_az = az),
+  #             by = .(tg = timegroup)][!is.na(focal_az)]
+  edges[DT, focal_az := az, on = .(ID1 == id, timegroup)]
+  # id_tg <- edges[DT, .(focal_az = az), on = .(ID1 == id, timegroup)]
+  # setnames(id_tg, 'timegroup', 'tg')
   id_tg[, {
-    DT[between(timegroup, tg - window, tg + window) & id != .BY$ID1][,
-       .(delay = tg - timegroup[which.min(focal_az - az)]),
-       by = .(ID2 = id)]
-  }, by = .(ID1, tg)]
+    DT[between(timegroup, tg - window, tg + window) & id == .BY$ID2][,
+       .(delay = tg - timegroup[which.min(focal_az - az)])]
+  }, by = .(ID1, ID2, tg)]
 }
 # Note: results must be saved like edge_ functions
 # Naming: edge_dir_delay?
