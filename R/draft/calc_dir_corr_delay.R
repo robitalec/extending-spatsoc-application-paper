959a8372295d094bc403f63b11f6da3b5c98b508
Alec L. Robitaille <robit.alec@gmail.com>
2024-06-24 20:31:01 -0300

    split off old function

--- R/draft/calc_dir_corr_delay.R
+++ R/draft/calc_dir_corr_delay.R
@@ -15,9 +15,13 @@ calc_dir_corr_delay <- function(DT, window) {
   # edges[DT, focal_az := az, on = .(ID1 == id, timegroup)]
   # id_tg <- edges[DT, .(focal_az = az), on = .(ID1 == id, timegroup)]
   # setnames(id_tg, 'timegroup', 'tg')
+  # TODO: returnDist = TRUE option and if TRUE then check col exists
   DT[, {
-    DT[between(timegroup, tg - window, tg + window) & ID2 != .BY$ID1][,
-       .(delay = tg - timegroup[which.min(diff_az)]),
+    DT[between(timegroup, tg - window, tg + window) & ID2 != .BY$ID1][,{
+      min_diff_az <- which.min(diff_az)
+      .(delay = tg - timegroup[min_diff_az],
+        distance = distance[min_diff_az])
+    },
        by = ID2]
   }, by = .(ID1, ID2, tg = timegroup)]
 }
@@ -28,3 +32,20 @@ calc_dir_corr_delay <- function(DT, window) {
 # TODO: check chmatch
 
 # TODO: is there some way to avoid double calculating, with dyadID?
+
+# TODO: if only one observation eg. run of 1 observations grouped
+#       what should we return?
+# TODO: profile
+
+
+calc_dir_corr_delay_from_DT <- function(DT, window) {
+  setorder(DT, timegroup)
+  id_tg <- DT[, .(ID1 = unique(id), focal_az = az),
+              by = .(tg = timegroup)][!is.na(focal_az)]
+  id_tg[, {
+    DT[between(timegroup, tg - window, tg + window) & id != .BY$ID1][,
+      # TODO: caution if timegroup = 1, delay will be -1 without any valid observations
+        .(delay = tg - timegroup[which.min(focal_az - az)]),
+         by = id]
+  }, by = .(ID1, tg)]
+}
