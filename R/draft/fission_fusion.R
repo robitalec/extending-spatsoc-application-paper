927401c02911af6bbfcdb02e8be88b1256549c0d
Alec L. Robitaille <robit.alec@gmail.com>
2024-06-27 13:08:11 -0300

    check distance within threshold

--- R/draft/fission_fusion.R
+++ R/draft/fission_fusion.R
@@ -3,3 +3,5 @@ fission_fusion <- function(edges, threshold = 50, min_run_len = 2,
   unique_edges <- unique(edges[, .(dyadID, timegroup, distance)])
 
   setorder(unique_edges, 'timegroup')
+
+  unique_edges[, within := distance < threshold]
