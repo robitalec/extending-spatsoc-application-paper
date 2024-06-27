e203acc993ee161620e95d531a236425576a49f7
Alec L. Robitaille <robit.alec@gmail.com>
2024-06-27 13:07:38 -0300

    more testing

--- tests/test-fission-fusion.R
+++ tests/test-fission-fusion.R
@@ -9,6 +9,7 @@ library(sf)
 library(lwgeom)
 library(spatsoc)
 library(testthat)
+library(patchwork)
 
 
 
@@ -19,9 +20,9 @@ targets::tar_source('R/draft')
 
 # Data --------------------------------------------------------------------
 DT_template <- data.table(
-  x = c(0, 10, 20, 30, 40, 50),
-  y = c(20, 1, 1, 15, 1, 15),
-  timegroup = seq.int(6),
+  x = c(0, 10, 20, 30, 40, 50, 60, 70),
+  y = c(20, 1, 1, 15, 1, 15, 15, 15),
+  timegroup = seq.int(8),
   id =  'A'
 )
 
@@ -37,6 +38,17 @@ DT_fogo <- fread('../prepare-locs/output/2024-01-26_NL-Fogo-Caribou-Telemetry.cs
 
 
 # Test --------------------------------------------------------------------
+setorder(DT_test, timegroup)
+group_pts(DT_test, threshold = 5, timegroup = 'timegroup',
+          coords = c('x', 'y'), id = 'id')
+
+edges <- edge_dist(DT_test, threshold = 50, id = 'id', timegroup = 'timegroup',
+                   coords = c('x', 'y'), returnDist = TRUE)
+dyad_id(edges, 'ID1', 'ID2')
+fiss_fus <- fission_fusion(edges, threshold = 10, min_run_len = 3)[]
+print(fiss_fus)
+
+fiss_fus[order(timegroup)]
 
 
 # Plot --------------------------------------------------------------------
@@ -44,7 +56,12 @@ g <- ggplot(DT_test, aes(x, y, color = id)) +
   geom_path(arrow = arrow()) +
   geom_label(aes(label = timegroup)) +
   theme_bw()
-print(g)
+g2 <- ggplot(fiss_fus,
+             aes(timegroup,  dyad_fusion_id, color = dyadID, group = dyad_fusion_id)) +
+  geom_line() +
+  geom_point() +
+  theme_bw()
+print(g / g2)
 
 
 
