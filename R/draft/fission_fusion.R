479ad00a7dd9b2ab5667c3245b5c384200dbdb35
Alec L. Robitaille <robit.alec@gmail.com>
2024-06-27 13:08:05 -0300

    unique by dyad, sort by time

--- R/draft/fission_fusion.R
+++ R/draft/fission_fusion.R
@@ -1,2 +1,5 @@
 fission_fusion <- function(edges, threshold = 50, min_run_len = 2,
                            max_missing_obs = 1)  {
+  unique_edges <- unique(edges[, .(dyadID, timegroup, distance)])
+
+  setorder(unique_edges, 'timegroup')
