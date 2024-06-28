539679e4fbb07684837fa9b315788e25ccca1975
Alec L. Robitaille <robit.alec@gmail.com>
2024-06-28 17:04:15 -0300

    more tests

--- tests/test_directional_correlation_delay.R
+++ tests/test_directional_correlation_delay.R
@@ -25,7 +25,7 @@ DT_test <- rbindlist(list(
   DT_template,
   DT_template[, .(x = x - 1, y = y - 1, timegroup = timegroup + 1, id = 'B')],
   DT_template[, .(x = x - 3, y = y - 3, timegroup = timegroup + 2, id = 'C')],
-  data.table(x = c(20, 20, -20), y = c(-20, -20, -20), timegroup = c(1, 2, 1),
+  data.table(x = c(30, 20, -20), y = c(-30, -20, -20), timegroup = c(1, 2, 1),
              id = c('C', 'C', 'B'))
 ))[timegroup > 0]
 setorder(DT_test, 'timegroup')
@@ -35,19 +35,16 @@ DT_fogo <- fread('../prepare-locs/output/2024-01-26_NL-Fogo-Caribou-Telemetry.cs
 
 
 # Test --------------------------------------------------------------------
-edges <- edge_dist(DT_test, threshold = 50, id = 'id', timegroup = 'timegroup',
-                   coords = c('x', 'y'), returnDist = TRUE, fillNA = FALSE)
+setorder(DT_test, timegroup)
+
+edges_test <- edge_dist(DT_test, threshold = 50, id = 'id', timegroup = 'timegroup',
+                        coords = c('x', 'y'), returnDist = TRUE)
+dyad_id(edges_test, 'ID1', 'ID2')
+fission_fusion(edges_test, threshold = 10, n_min_length = 1, n_max_missing = 1)
 calc_az(DT_test, coords = c('x', 'y'), projection = 4326)
-dyad_id(edges, 'ID1', 'ID2')
-fiss_fus <- fission_fusion(edges, threshold = 20,
-                           min_run_len = 1, n_max_missing = 0)
 
-calc_dir_corr_delay(DT_test, fiss_fus, window = 2)
+calc_dir_corr_delay(DT_test, edges_test, window = 2)[]
 
-
-edge_test <- edge_az(DT_test, NULL, id = 'id', coords = c('x', 'y'),
-                     timegroup = 'timegroup', returnDist = TRUE)
-calc_dir_corr_delay(edge_test, window = 3) |> print()
 
 
 # Test where exaggerated window size
