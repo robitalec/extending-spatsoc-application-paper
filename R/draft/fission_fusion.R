3db590bad6b025cd2d2916539c66a2e0e71f3dde
Alec L. Robitaille <robit.alec@gmail.com>
2024-06-28 16:06:31 -0300

    rename col to fusionID

--- R/draft/fission_fusion.R
+++ R/draft/fission_fusion.R
@@ -7,19 +7,19 @@ fission_fusion <- function(edges, threshold = 50,
 
   unique_edges[, within := distance < threshold]
   unique_edges[, tg_diff := shift(timegroup, -1, fill = -999) - timegroup, by = dyadID]
-  unique_edges[, runID := fifelse(
+  unique_edges[, fusionID := fifelse(
     within,
     rleid((tg_diff <= 1 + n_max_missing)),
     NA_integer_),
   by = dyadID]
 
   if (!is.null(min_run_len)) {
-    unique_edges[!is.na(runID),
-                 runID := fifelse(.N >= min_run_len, runID, NA_integer_),
-                 by = .(dyadID, runID)]
+    unique_edges[!is.na(fusionID),
+                 fusionID := fifelse(.N >= min_run_len, fusionID, NA_integer_),
+                 by = .(dyadID, fusionID)]
   }
 
-  unique_edges[!is.na(runID), runID := .GRP, by = .(dyadID, runID)]
+  unique_edges[!is.na(fusionID), fusionID := .GRP, by = .(dyadID, fusionID)]
   unique_edges[, c('within', 'tg_diff') := NULL]
 
   return(unique_edges)
