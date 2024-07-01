47288ca2fdf376bd5ef4948bacff1b2d73646d38
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-01 11:26:43 -0300

    rm old

--- R/draft/calc_dir_corr_delay.R
+++ R/draft/calc_dir_corr_delay.R
@@ -28,29 +28,11 @@ calc_dir_corr_delay <- function(DT, edges, window) {
     focal_az <- DT[timegroup == .BY$tg & id == ID1, az]
     DT[between(timegroup, min_tg, max_tg) & id == ID2,
        timegroup[which.min(abs(focal_az -  az))]]
-    # DT[between(timegroup, tg - window, tg + window) & id != .BY$ID1][,
-     # TODO: caution if timegroup = 1, delay will be -1 without any valid observations
-     # .(delay = tg - timegroup[which.min(focal_az - az)]),
-     # by = id]
-  }, by = .(dyadID, tg)]
 
   # TODO more fifelse
 
-  # id_tg <- DT[, .(ID1 = unique(ID1)),
-  #             by = .(tg = timegroup)]#[!is.na(focal_az)]
-  # edges[DT, focal_az := az, on = .(ID1 == id, timegroup)]
-  # id_tg <- edges[DT, .(focal_az = az), on = .(ID1 == id, timegroup)]
-  # setnames(id_tg, 'timegroup', 'tg')
-  # TODO: returnDist = TRUE option and if TRUE then check col exists
-  # DT[, {
-  #   DT[between(timegroup, tg - window, tg + window) & ID2 != .BY$ID1][,{
-  #     min_diff_az <- which.min(diff_az)
-  #     .(delay = tg - timegroup[min_diff_az],
-  #       distance = distance[min_diff_az])
-  #   },
-  #      by = ID2]
-  # }, by = .(ID1, ID2, tg = timegroup)]
 }
+
 # Note: results must be saved like edge_ functions
 # Naming: edge_dir_delay?
 # TODO: consider using new env() functionality from data.table
@@ -61,17 +43,3 @@ calc_dir_corr_delay <- function(DT, edges, window) {
 
 # TODO: if only one observation eg. run of 1 observations grouped
 #       what should we return?
-# TODO: profile
-
-
-calc_dir_corr_delay_from_DT <- function(DT, window) {
-  setorder(DT, timegroup)
-  id_tg <- DT[, .(ID1 = unique(id), focal_az = az),
-              by = .(tg = timegroup)][!is.na(focal_az)]
-  id_tg[, {
-    DT[between(timegroup, tg - window, tg + window) & id != .BY$ID1][,
-      # TODO: caution if timegroup = 1, delay will be -1 without any valid observations
-        .(delay = tg - timegroup[which.min(focal_az - az)]),
-         by = id]
-  }, by = .(ID1, tg)]
-}
