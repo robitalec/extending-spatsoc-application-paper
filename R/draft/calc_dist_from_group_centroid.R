bc838afd0845cce42ed65dae2f4d517c92b364fa
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-04 17:18:47 -0300

    use var for pre cols, check if present

--- R/draft/calc_dist_from_group_centroid.R
+++ R/draft/calc_dist_from_group_centroid.R
@@ -1,7 +1,16 @@
 calc_dist_from_group_centroid <- function(DT, xcol, ycol) {
   pre <- 'group_mean_'
+  group_xcol <- paste0(pre, xcol)
+  group_ycol <- paste0(pre, ycol)
+
+  stopifnot(xcol %in% colnames(DT))
+  stopifnot(ycol %in% colnames(DT))
+  stopifnot(group_xcol %in% colnames(DT))
+  stopifnot(group_ycol %in% colnames(DT))
+
+
   DT[, dist_from_group_centroid :=
-       sqrt((.SD[[xcol]] - .SD[[paste0(pre, xcol)]])^2 +
-              (.SD[[ycol]] - .SD[[paste0(pre, ycol)]])^2)]
+       sqrt((.SD[[xcol]] - .SD[[group_xcol]])^2 +
+              (.SD[[ycol]] - .SD[[group_ycol]])^2)]
   return(DT[])
 }
