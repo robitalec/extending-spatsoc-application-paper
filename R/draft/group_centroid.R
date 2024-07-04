704474877d08d982f39e7b51824123c0ff908c6c
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-04 17:10:53 -0300

    pass na rm arg

--- R/draft/group_centroid.R
+++ R/draft/group_centroid.R
@@ -1,4 +1,4 @@
-group_centroid <- function(DT, xcol, ycol, group = 'group') {
+group_centroid <- function(DT, xcol, ycol, group = 'group', na.rm = FALSE) {
   stopifnot(xcol %in% colnames(DT))
   stopifnot(ycol %in% colnames(DT))
   stopifnot(group %in% colnames(DT))
