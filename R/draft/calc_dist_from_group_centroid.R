54156c94faaa81454f572c836b09bf8145598a64
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-04 17:17:06 -0300

    calc dist

--- R/draft/calc_dist_from_group_centroid.R
+++ R/draft/calc_dist_from_group_centroid.R
@@ -1,2 +1,7 @@
 calc_dist_from_group_centroid <- function(DT, xcol, ycol) {
   pre <- 'group_mean_'
+  DT[, dist_from_group_centroid :=
+       sqrt((.SD[[xcol]] - .SD[[paste0(pre, xcol)]])^2 +
+              (.SD[[ycol]] - .SD[[paste0(pre, ycol)]])^2)]
+  return(DT[])
+}
