4bc51746d33a99bbbf5327bf5d9893558c1569a7
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-09 16:40:31 -0300

    test manual

--- tests/test-position-within-group.R
+++ tests/test-position-within-group.R
@@ -33,15 +33,11 @@ DT_fogo <- fread('../prepare-locs/output/2024-01-26_NL-Fogo-Caribou-Telemetry.cs
 
 
 
-
 # Test --------------------------------------------------------------------
-group_pts(DT_test, 5, 'id', c('x', 'y'), timegroup = 'timegroup')
-calc_az(DT_test, coords = c('x', 'y'), projection = 4326)
-group_centroid(DT_test, 'x', 'y', 'group')
+theta <- CircStats::rad(45)
+origin <- c(2, 1)
+xy <- c(4, 7)
+mat <- matrix(c(cos(theta), -sin(theta), sin(theta), cos(theta)),
+              byrow = TRUE, ncol = 2)
 
-DT_test[, group_az := mean(az), by = group]
-theta <- DT_test[, first(group_az)]
-origin <- c(DT_test[1, group_mean_x], DT_test[1, group_mean_y])
-(matrix(c(cos(theta), -sin(theta), sin(theta), cos(theta)), byrow = TRUE, ncol = 2)
-  %*% (c(2, 4) - origin)) #+
-  # origin
+print(mat %*% (xy - origin))
