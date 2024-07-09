1d80ff452d87572fd07ad46ceaf9ff9a6f5e64bc
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-09 16:01:18 -0300

    check for cols

--- R/draft/position_within_group.R
+++ R/draft/position_within_group.R
@@ -1,3 +1,7 @@
 position_within_group <- function(DT, coords = c('x', 'y')) {
+  stopifnot('az' %in% colnames(DT))
+  stopifnot('group' %in% colnames(DT))
+  stopifnot('group_mean_x' %in% colnames(DT))
+  stopifnot('group_mean_y' %in% colnames(DT))
 
 }
