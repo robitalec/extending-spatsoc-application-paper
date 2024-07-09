2cdd0a9aeb743becf97e00ec145d2b8e6fd129a3
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-09 18:54:59 -0300

    return second col

--- R/draft/position_within_group.R
+++ R/draft/position_within_group.R
@@ -8,11 +8,10 @@ position_within_group <- function(DT, coords = c('x', 'y')) {
   DT[, group_az := mean(az), by = group]
 
   DT[, dist_along_group_az :=
-       last(matrix(c(cos(.SD[['group_az']]), -sin(.SD[['group_az']]),
+       (matrix(c(cos(.SD[['group_az']]), -sin(.SD[['group_az']]),
                  sin(.SD[['group_az']]), cos(.SD[['group_az']])),
                byrow = TRUE, ncol = 2) %*%
-          (c(.SD[[2]] - .SD[[4]], .SD[[3]] - .SD[[5]]))),
+          (c(.SD[[2]] - .SD[[4]], .SD[[3]] - .SD[[5]])))[2,],
      .SDcols = c('group_az', coords, paste0('group_mean_', coords)),
-     by = group]
 
 }
