f9f403b9633a7371a4534d87da137d8e39672265
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-08 23:11:43 -0300

    rotation matrix

--- tests/test-position-within-group.R
+++ tests/test-position-within-group.R
@@ -24,13 +24,21 @@ DT_template <- data.table(
   y = c(20, 1, 1, 15, 1, 15, 15, 15),
   timegroup = seq.int(8),
   id =  'A'
+DT_test <- data.table(
+  x = 2,
+  y = 1,
+  group_mean_x = 1,
+  group_mean_y = 1,
+  group_az = 1.3
 )
 
-DT_test <- rbindlist(list(
-  DT_template,
-  DT_template[, .(x, y = y * -1, timegroup, id = 'B')],
-  DT_template[, .(x, y = y - 5, timegroup, id = 'C')]
-))[!(id == 'C' & timegroup %in% c(3, 5, 6))]
+DT_test <- data.table(
+  x = 2,
+  y = 4,
+  group_mean_x = 1,
+  group_mean_y = 1,
+  group_az = 0.6435
+)
 
 
 DT_fogo <- fread('../prepare-locs/output/2024-01-26_NL-Fogo-Caribou-Telemetry.csv')
@@ -44,3 +52,8 @@ calc_az(DT_test, coords = c('x', 'y'), projection = 4326)
 group_centroid(DT_test, 'x', 'y', 'group')
 
 DT_test[, group_az := mean(az), by = group]
+theta <- DT_test[, first(group_az)]
+origin <- c(DT_test[1, group_mean_x], DT_test[1, group_mean_y])
+(matrix(c(cos(theta), -sin(theta), sin(theta), cos(theta)), byrow = TRUE, ncol = 2)
+  %*% (c(2, 4) - origin)) #+
+  # origin
