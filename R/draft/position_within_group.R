911f8da45a8a95fa39c8aa2a2ec01bb82c7bb655
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-09 16:39:54 -0300

    fix check with group_mean_ paste

--- R/draft/position_within_group.R
+++ R/draft/position_within_group.R
@@ -1,8 +1,8 @@
 position_within_group <- function(DT, coords = c('x', 'y')) {
   stopifnot('az' %in% colnames(DT))
   stopifnot('group' %in% colnames(DT))
-  stopifnot('group_mean_x' %in% colnames(DT))
-  stopifnot('group_mean_y' %in% colnames(DT))
+  stopifnot(paste0('group_mean_', coords[1]) %in% colnames(DT))
+  stopifnot(paste0('group_mean_', coords[2]) %in% colnames(DT))
   # TODO: check if az in radians not degrees
 
   DT[, group_az := mean(az), by = group]
