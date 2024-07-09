619b493fd8ac1b34d6d34bfaf1a6cc2a286f40c7
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-09 16:41:05 -0300

    fix return column only dist along axis

--- R/draft/position_within_group.R
+++ R/draft/position_within_group.R
@@ -7,10 +7,12 @@ position_within_group <- function(DT, coords = c('x', 'y')) {
 
   DT[, group_az := mean(az), by = group]
 
-  DT[, #dist_along_group_az :=
-       (matrix(c(cos(group_az), -sin(group_az), sin(group_az), cos(group_az)),
+  DT[, dist_along_group_az :=
+       last(matrix(c(cos(.SD[['group_az']]), -sin(.SD[['group_az']]),
+                 sin(.SD[['group_az']]), cos(.SD[['group_az']])),
                byrow = TRUE, ncol = 2) %*%
-          (c(.SD[[1]], .SD[[2]]) - c(.SD[[3]], .SD[[4]]))),
-     .SDcols = c('group_az', coords, paste0('group_mean_', coords))]
+          (c(.SD[[2]] - .SD[[4]], .SD[[3]] - .SD[[5]]))),
+     .SDcols = c('group_az', coords, paste0('group_mean_', coords)),
+     by = group]
 
 }
