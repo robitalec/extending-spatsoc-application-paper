621a906c2a2f03e6aa3b92f09ab492c8b82b4240
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-10 11:48:47 -0300

    use vars for x, y and group x,y cols

--- R/draft/position_within_group.R
+++ R/draft/position_within_group.R
@@ -1,8 +1,16 @@
 position_within_group <- function(DT, coords = c('x', 'y')) {
+
+  xcol <- first(coords)
+  ycol <- last(coords)
+  xcol_group <- paste0('group_mean_', xcol)
+  ycol_group <- paste0('group_mean_', ycol)
+
   stopifnot('az' %in% colnames(DT))
   stopifnot('group' %in% colnames(DT))
-  stopifnot(paste0('group_mean_', coords[1]) %in% colnames(DT))
-  stopifnot(paste0('group_mean_', coords[2]) %in% colnames(DT))
+  stopifnot(xcol %in% colnames(DT))
+  stopifnot(ycol %in% colnames(DT))
+  stopifnot(xcol_group %in% colnames(DT))
+  stopifnot(ycol_group %in% colnames(DT))
   # TODO: check if az in radians not degrees
 
   DT[, group_az := mean(az), by = group]
