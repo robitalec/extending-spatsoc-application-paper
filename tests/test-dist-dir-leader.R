dd61aff04742d6c9f41aff250ed6432990821ee2
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-11 11:58:17 -0300

    test calc dist fogo

--- tests/test-dist-dir-leader.R
+++ tests/test-dist-dir-leader.R
@@ -49,3 +49,8 @@ group_centroid(DT_fogo, first(coords), last(coords))
 calc_az(DT_fogo, c('x_long', 'y_lat'), 4326)
 
 position_within_group(DT_fogo, coords = coords, return_rank = TRUE)
+calc_dist_to_leader(DT_fogo, coords = coords, group = 'group')
+print(DT_fogo[group == DT_fogo[, .N, group][N > 3, sample(group, 1)],
+              .(id, timegroup, group, x_proj, y_proj, group_mean_x_proj,
+                group_az, dist_along_group_az, rank_dist_along_group_az,
+                dist_to_leader)])
