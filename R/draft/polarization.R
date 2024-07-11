1b76ec82b4d7ac073f75d7e8177879ff38f8fd5e
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-11 15:27:23 -0300

    calc polarization

--- R/draft/polarization.R
+++ R/draft/polarization.R
@@ -1,1 +1,8 @@
 calc_polarization <- function(DT,  group = 'group') {
+  stopifnot('az' %in% colnames(DT))
+  stopifnot('group' %in% colnames(DT))
+
+  DT[, polarization := r.test(.SD)$r.bar, by = c(group), .SDcols = c('az')]
+
+  return(DT)
+}
