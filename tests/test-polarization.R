f2fe654abe0a14a445aae99d069e2a3e8ba6bcc9
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-11 15:27:31 -0300

    fst test calc polarization

---
+++ tests/test-polarization.R
@@ -0,0 +1,32 @@
+# === Test polarization ---------------------------------------------------
+
+
+
+# Packages ----------------------------------------------------------------
+library(data.table)
+library(ggplot2)
+library(sf)
+library(lwgeom)
+library(spatsoc)
+library(testthat)
+library(patchwork)
+library(ggdist)
+
+
+# Functions ---------------------------------------------------------------
+targets::tar_source('R/draft')
+
+
+
+# Data --------------------------------------------------------------------
+n <- 10
+DT_test <- data.table(
+  x = runif(n, -10, 10),
+  y = runif(n, -10, 10),
+  id = LETTERS[seq.int(n)],
+  az = runif(n, CircStats::rad(0), CircStats::rad(360)),
+  group = 1
+)
+
+DT_fogo <- fread('../prepare-locs/output/2024-01-26_NL-Fogo-Caribou-Telemetry.csv')
+
