29b9569e3345c75c233802081db45640847336f6
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-11 12:52:28 -0300

    plot dir dist to leader fogo

--- tests/test-dist-dir-leader.R
+++ tests/test-dist-dir-leader.R
@@ -79,3 +79,39 @@ g <- ggplot(DT_test, aes(x, y, color = id)) +
 
 print(g)
 
+
+sel_group <- DT_fogo[N_by_group > 6, sample(group, 1)]
+sub_fogo <- DT_fogo[group == sel_group]
+slope_fogo <- sub_fogo[1, tan(group_az)]
+intercept_fogo <- sub_fogo[1, group_mean_y_proj - slope_fogo * group_mean_x_proj]
+intercept_inv_fogo <- sub_fogo[1, group_mean_y_proj - (-1/slope_fogo * group_mean_x_proj)]
+
+g_fogo <- ggplot(sub_fogo, aes(x_proj, y_proj, color = id)) +
+  geom_point(size = 0.8) +
+  geom_text(aes(label = rank_dist_along_group_az), nudge_y = 1.5) +
+  geom_text(aes(label = paste0(format(dist_to_leader, digits = 2),
+                               ', ',
+                               format(dir_to_leader, digits = 2),
+                               ' rad')), nudge_y = -1.5) +
+  geom_point(color = 'black', aes(group_mean_x_proj, group_mean_y_proj)) +
+  geom_abline(slope = slope_fogo, intercept = intercept_fogo) +
+  geom_abline(slope = -1/slope_fogo, intercept = intercept_inv_fogo,
+              linewidth = 0.2) +
+  theme_bw() +
+  labs(x = '', y = '') +
+  theme(axis.text = element_blank(), axis.ticks = element_blank()) +
+  coord_fixed() +
+  guides(color = 'none') +
+  scale_x_continuous(expand = expansion(add = 10))
+
+g_dist <- ggplot(DT_fogo, aes(dist_to_leader, factor(rank_dist_along_group_az))) +
+  stat_halfeye() +
+  labs(x = 'Direction to leader', y = 'Rank along group az') +
+  theme_bw()
+
+g_dir <- ggplot(DT_fogo, aes(dir_to_leader, factor(rank_dist_along_group_az))) +
+  stat_halfeye() +
+  labs(x = 'Distance to leader', y = 'Rank along group az') +
+  theme_bw()
+
+print(g_fogo / (g_dist + g_dir))
