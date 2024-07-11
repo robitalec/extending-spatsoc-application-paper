cab43cec2b63eb9ee6a24ec1da7e9da417bc109f
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-11 12:39:23 -0300

    calc dir to leader

--- R/draft/calc_dir_to_leader.R
+++ R/draft/calc_dir_to_leader.R
@@ -3,3 +3,26 @@ calc_dirs_to_leader <- function(DT, coords = c('x', 'y'), group = 'group') {
   stopifnot(last(coords) %in% colnames(DT))
   stopifnot(group %in% colnames(DT))
   stopifnot('rank_dist_along_group_az' %in% colnames(DT))
+
+  check_has_leader <- DT[, .(has_leader = any(rank_dist_along_group_az == 1)),
+                         by = c(group)][!(has_leader)]
+
+  if (check_has_leader[, .N > 0]) {
+    warning('groups found missing leader (rank_dist_along_group_az == 1): \n',
+            check_has_leader[, paste(group, collapse = ', ')])
+  }
+
+  DT[, temp_leader_xcol := .SD[which(rank_dist_along_group_az == 1)],
+     .SDcols = first(coords), by = c(group)]
+  DT[, temp_leader_ycol := .SD[which(rank_dist_along_group_az == 1)],
+     .SDcols = last(coords), by = c(group)]
+
+  DT[!group %in% check_has_leader$group, dir_to_leader := fifelse(
+    .SD[[xcol]] == .SD[['temp_leader_xcol']] &
+      .SD[[ycol]] == .SD[['temp_leader_ycol']],
+    NaN,
+    atan2(.SD[['temp_leader_xcol']] - .SD[[xcol]],
+          (.SD[['temp_leader_ycol']] - .SD[[ycol]]))
+  )]
+  return(DT)
+}
