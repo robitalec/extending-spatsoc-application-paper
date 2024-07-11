e30368b1a017f53bd55699c3ecd46782f6f2d85b
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-11 11:49:33 -0300

    use temporary N by group to only calc where gt 1

--- R/draft/calc_dist_to_leader.R
+++ R/draft/calc_dist_to_leader.R
@@ -4,10 +4,12 @@ calc_dist_to_leader <- function(DT, coords = c('x', 'y'), group = 'group') {
   stopifnot(group %in% colnames(DT))
   stopifnot('rank_dist_along_group_az' %in% colnames(DT))
 
-  DT[, dist_to_leader :=
-       as.matrix(dist(cbind(.SD[order(rank_dist_along_group_az)]), diag = TRUE))[, 1],
+  DT[, temp_N_by_group := .N, by = c(group)]
+  DT[, dist_to_leader := fifelse(temp_N_by_group > 1,
+                                 as.matrix(dist(cbind(.SD[order(rank_dist_along_group_az)]), diag = TRUE))[, 1],
+                                 0),
      .SDcols = c(coords),
      by = c(group)]
-
+  DT[, temp_N_by_group := NULL]
   return(DT)
 }
