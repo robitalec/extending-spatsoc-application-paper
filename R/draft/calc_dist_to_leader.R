2b3b215aa64d4a259ebe9a385276e5a0ee307459
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-11 11:53:45 -0300

    try set order

--- R/draft/calc_dist_to_leader.R
+++ R/draft/calc_dist_to_leader.R
@@ -5,8 +5,9 @@ calc_dist_to_leader <- function(DT, coords = c('x', 'y'), group = 'group') {
   stopifnot('rank_dist_along_group_az' %in% colnames(DT))
 
   DT[, temp_N_by_group := .N, by = c(group)]
+  setorder(DT, 'rank_dist_along_group_az')
   DT[, dist_to_leader := fifelse(temp_N_by_group > 1,
-                                 as.matrix(dist(cbind(.SD[order(rank_dist_along_group_az)]), diag = TRUE))[, 1],
+                                 as.matrix(dist(cbind(.SD), diag = TRUE))[, 1],
                                  0),
      .SDcols = c(coords),
      by = c(group)]
