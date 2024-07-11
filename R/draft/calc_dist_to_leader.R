ddc5aead6e218057abe9056d50f8dd60da63c2b3
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-11 11:49:12 -0300

    calc dist to leader

--- R/draft/calc_dist_to_leader.R
+++ R/draft/calc_dist_to_leader.R
@@ -3,3 +3,8 @@ calc_dist_to_leader <- function(DT, coords = c('x', 'y'), group = 'group') {
   stopifnot(last(coords) %in% colnames(DT))
   stopifnot(group %in% colnames(DT))
   stopifnot('rank_dist_along_group_az' %in% colnames(DT))
+
+  DT[, dist_to_leader :=
+       as.matrix(dist(cbind(.SD[order(rank_dist_along_group_az)]), diag = TRUE))[, 1],
+     .SDcols = c(coords),
+     by = c(group)]
