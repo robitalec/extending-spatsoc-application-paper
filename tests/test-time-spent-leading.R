01bac6818b9b2a5cfb46348d96871962679254bf
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-11 09:56:22 -0300

    setup from test position within group

--- tests/test-time-spent-leading.R
+++ tests/test-time-spent-leading.R
@@ -1,3 +1,89 @@
 # === Test time spent leading ---------------------------------------------
 
 
+
+# Packages ----------------------------------------------------------------
+library(data.table)
+library(ggplot2)
+library(sf)
+library(lwgeom)
+library(spatsoc)
+library(testthat)
+library(patchwork)
+library(ggdist)
+
+
+# Functions ---------------------------------------------------------------
+targets::tar_source('R/draft')
+
+
+
+# Data --------------------------------------------------------------------
+n <- 10
+DT_test <- data.table(
+  x = runif(n, -10, 10),
+  y = runif(n, -10, 10),
+  id = LETTERS[seq.int(n)],
+  az = runif(n, CircStats::rad(0), CircStats::rad(360)),
+  timegroup = 1,
+  group = 1
+)
+
+DT_fogo <- fread('../prepare-locs/output/2024-01-26_NL-Fogo-Caribou-Telemetry.csv')
+
+
+
+# Test --------------------------------------------------------------------
+group_centroid(DT_test, 'x', 'y')
+position_within_group(DT_test, coords = c('x', 'y'), return_rank = TRUE)
+threshold <- 50
+DT_fogo[, datetime := as.POSIXct(datetime, tz = 'UTC')]
+group_times(DT_fogo, datetime = 'datetime', threshold = '20 minutes')
+group_pts(DT_fogo, threshold = threshold, id = 'id',
+          coords = c('x_proj', 'y_proj'), timegroup = 'timegroup')
+group_centroid(DT_fogo, 'x_proj', 'y_proj')
+calc_az(DT_fogo, c('x_long', 'y_lat'), 4326)
+
+position_within_group(DT_fogo, coords = c('x_proj', 'y_proj'),
+                      return_rank = TRUE)
+print(DT_fogo[group == DT_fogo[, .N, group][N > 3, sample(group, 1)],
+              .(id, timegroup, group, x_proj, y_proj, group_mean_x_proj,
+                group_az, dist_along_group_az, rank_dist_along_group_az)])
+# Plot --------------------------------------------------------------------
+slope <- DT_test[1, tan(group_az)]
+intercept <- DT_test[1, group_mean_y - slope * group_mean_x]
+intercept_inv <-  DT_test[1, group_mean_y - (-1/slope) * group_mean_x]
+g <- ggplot(DT_test, aes(x, y, color = id)) +
+  geom_abline(slope = slope, intercept = intercept) +
+  geom_abline(slope = -1/slope, intercept = intercept_inv, linewidth = 0.3) +
+  geom_point() +
+  geom_text(aes(label = rank_dist_along_group_az), nudge_y = 0.5) +
+  geom_point(color = 'black', aes(group_mean_x, group_mean_y)) +
+  theme_bw() +
+  lims(x = c(-10, 10), y = c(-10, 10)) +
+  coord_fixed() +
+  guides(color = 'none')
+
+print(g)
+
+
+sel_group <- DT_fogo[N_by_group > 6, sample(group, 1)]
+sub_fogo <- DT_fogo[group == sel_group]
+slope_fogo <- sub_fogo[1, tan(group_az)]
+intercept_fogo <- sub_fogo[1, group_mean_y_proj - slope_fogo * group_mean_x_proj]
+intercept_inv_fogo <- sub_fogo[1, group_mean_y_proj - (-1/slope_fogo * group_mean_x_proj)]
+
+g_fogo <- ggplot(sub_fogo, aes(x_proj, y_proj, color = id)) +
+  geom_point(size = 0.8) +
+  geom_text(aes(label = rank_dist_along_group_az), nudge_y = 2) +
+  geom_point(color = 'black', aes(group_mean_x_proj, group_mean_y_proj)) +
+  geom_abline(slope = slope_fogo, intercept = intercept_fogo) +
+  geom_abline(slope = -1/slope_fogo, intercept = intercept_inv_fogo,
+              linewidth = 0.2) +
+  theme_bw() +
+  labs(x = '', y = '') +
+  theme(axis.text = element_blank(), axis.ticks = element_blank()) +
+  coord_fixed() +
+  guides(color = 'none') +
+  scale_x_continuous(expand = expansion(add = 10))
+
