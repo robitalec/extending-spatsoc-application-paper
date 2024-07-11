de800760151422f8db7a11ebe0c190bc557a7319
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-11 11:57:41 -0300

    use which rank eq 1

--- R/draft/calc_dist_to_leader.R
+++ R/draft/calc_dist_to_leader.R
@@ -5,10 +5,12 @@ calc_dist_to_leader <- function(DT, coords = c('x', 'y'), group = 'group') {
   stopifnot('rank_dist_along_group_az' %in% colnames(DT))
 
   DT[, temp_N_by_group := .N, by = c(group)]
-  DT[, dist_to_leader := fifelse(temp_N_by_group > 1,
-                                 as.matrix(dist(cbind(.SD), diag = TRUE))[, 1],
+
+  DT[, dist_to_leader := fifelse(
+    temp_N_by_group > 1,
+    as.matrix(dist(cbind(.SD[[1]], .SD[[2]])))[, which(.SD[[3]] == 1)],
                                  0),
-     .SDcols = c(coords),
+     .SDcols = c(coords, 'rank_dist_along_group_az'),
      by = c(group)]
   DT[, temp_N_by_group := NULL]
   return(DT)
