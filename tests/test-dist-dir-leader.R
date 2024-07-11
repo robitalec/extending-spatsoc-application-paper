465c387e3c8a64b00f082bbfc46ceb8671ba6612
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-11 12:46:14 -0300

    test calc dir

--- tests/test-dist-dir-leader.R
+++ tests/test-dist-dir-leader.R
@@ -36,7 +36,8 @@ DT_fogo <- fread('../prepare-locs/output/2024-01-26_NL-Fogo-Caribou-Telemetry.cs
 # Test --------------------------------------------------------------------
 group_centroid(DT_test, 'x', 'y')
 position_within_group(DT_test, coords = c('x', 'y'), return_rank = TRUE)
-calc_dist_to_leader(DT_test, coords = c('x', 'y'), group = 'group')[]
+calc_dist_to_leader(DT_test, coords = c('x', 'y'), group = 'group')
+calc_dir_to_leader(DT_test, coords = c('x', 'y'), group = 'group')[]
 
 
 threshold <- 50
@@ -50,7 +51,8 @@ calc_az(DT_fogo, c('x_long', 'y_lat'), 4326)
 
 position_within_group(DT_fogo, coords = coords, return_rank = TRUE)
 calc_dist_to_leader(DT_fogo, coords = coords, group = 'group')
+calc_dir_to_leader(DT_fogo, coords = coords, group = 'group')
 print(DT_fogo[group == DT_fogo[, .N, group][N > 3, sample(group, 1)],
               .(id, timegroup, group, x_proj, y_proj, group_mean_x_proj,
                 group_az, dist_along_group_az, rank_dist_along_group_az,
-                dist_to_leader)])
+                dist_to_leader, dir_to_leader)])
