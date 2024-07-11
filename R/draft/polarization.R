f979653811ba289487db383e0baabf06ad33fbf5
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-11 16:09:21 -0300

    alternative definition?

--- R/draft/polarization.R
+++ R/draft/polarization.R
@@ -2,7 +2,11 @@ calc_polarization <- function(DT,  group = 'group') {
   stopifnot('az' %in% colnames(DT))
   stopifnot('group' %in% colnames(DT))
 
+  # Klamser 2021: polarization = absolute value of the mean heading direction
+  # DT[, polarization := abs(mean(az)), by = c(group)]
+
   DT[, polarization := r.test(.SD)$r.bar, by = c(group), .SDcols = c('az')]
+
 
   return(DT)
 }
