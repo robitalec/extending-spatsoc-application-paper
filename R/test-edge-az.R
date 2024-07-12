a199b50f87b7f3b382b361eecee402250cc5f3b9
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-12 11:11:38 -0300

    fst test edge az

---
+++ R/test-edge-az.R
@@ -0,0 +1,39 @@
+# === Test edge az --------------------------------------------------------
+
+
+
+# Packages ----------------------------------------------------------------
+library(data.table)
+library(ggplot2)
+library(sf)
+library(lwgeom)
+library(spatsoc)
+library(testthat)
+library(patchwork)
+
+
+
+# Functions ---------------------------------------------------------------
+targets::tar_source('R/draft')
+
+
+
+# Data --------------------------------------------------------------------
+DT_template <- data.table(
+  x = seq(10, 80, by = 10),
+  y = c(20, 1, 1, 15, 1, 15, 15, 15),
+  timegroup = seq.int(8),
+  id =  'A'
+)
+
+DT_test <- rbindlist(list(
+  DT_template,
+  DT_template[, .(x, y = y * -1, timegroup, id = 'B')],
+  DT_template[, .(x, y = y - 5, timegroup, id = 'C')]
+))[!(id == 'C' & timegroup %in% c(3, 5, 6))]
+
+
+DT_fogo <- fread('../prepare-locs/output/2024-01-26_NL-Fogo-Caribou-Telemetry.csv')
+
+
+
