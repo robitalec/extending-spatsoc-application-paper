fa6f2c37f48d17dfe71164becbc478b45f570148
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-12 15:18:53 -0300

    mv to test-delta-rad

--- R/draft/delta_rad.R
+++ R/draft/delta_rad.R
@@ -9,14 +9,3 @@ delta_rad <- function(target, source,  signed = FALSE) {
   }
 }
 
-expect_equal(delta_rad(0.1, 0.2, TRUE), 0.1)
-expect_equal(delta_rad(0.1, 0.2 + 2 * pi, TRUE), 0.1)
-expect_equal(delta_rad(0.1, 0.2 - 2 * pi, TRUE), 0.1)
-expect_equal(delta_rad(0.1 + 2 * pi, 0.2, TRUE), 0.1)
-expect_equal(delta_rad(0.1 - 2 * pi, 0.2, TRUE), 0.1)
-expect_equal(delta_rad(0.2, 0.1, TRUE), -0.1)
-expect_equal(delta_rad(0.2 + 2 * pi, 0.1, TRUE), -0.1)
-expect_equal(delta_rad(0.2 - 2 * pi, 0.1, TRUE), -0.1)
-expect_equal(delta_rad(0.2, 0.1 + 2 * pi, TRUE), -0.1)
-expect_equal(delta_rad(0.2, 0.1 - 2 * pi, TRUE), -0.1)
-
