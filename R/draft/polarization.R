66f4b982a9b2f0d100ab6a6b2093d6b857c44a5c
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-17 17:27:25 -0300

    tidy

--- R/draft/polarization.R
+++ R/draft/polarization.R
@@ -8,12 +8,8 @@ calc_polarization <- function(DT,  group = 'group') {
   stopifnot('az' %in% colnames(DT))
   stopifnot('group' %in% colnames(DT))
 
-  # Klamser 2021: polarization = absolute value of the mean heading direction
-  # DT[, polarization := abs(mean(az)), by = c(group)]
-
   DT[, polarization := CircStats::r.test(.SD[[1]])$r.bar, by = c(group),
      .SDcols = c('az')]
-
 
   return(DT)
 }
