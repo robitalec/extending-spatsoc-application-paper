a5896b8ea291467afd7b4bca1742926cabf86771
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-17 17:03:09 -0300

    add check length coords

--- R/draft/group_centroid.R
+++ R/draft/group_centroid.R
@@ -1,10 +1,13 @@
 group_centroid <- function(DT, coords, group = 'group', na.rm = FALSE) {
+  stopifnot(length(coords) == 2)
+
   xcol <- first(coords)
   ycol <- last(coords)
 
   stopifnot(xcol %in% colnames(DT))
   stopifnot(ycol %in% colnames(DT))
   stopifnot(group %in% colnames(DT))
+
   DT[, paste0('group_mean_', xcol) := mean(.SD[[xcol]], na.rm = na.rm), by = c(group)]
   DT[, paste0('group_mean_', ycol) := mean(.SD[[ycol]], na.rm = na.rm), by = c(group)]
   return(DT[])
