c75ef80aaf5600a077286994930ebf094b7654b9
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-17 13:32:02 -0300

    clean

--- R/draft/calc_az.R
+++ R/draft/calc_az.R
@@ -24,18 +24,3 @@ calc_az <- function(DT, id = NULL, coords = NULL, projection = NULL) {
 
   }
 }
-
-# https://postgis.net/docs/ST_Azimuth.html
-
-
-# TODO: lead/lag?
-
-# TODO: NaN return
-# R: p = st_sfc(st_point(c(7,52)), st_point(c(8,53)), st_point(c(8,53)), crs = 4326)
-# R: st_geod_azimuth(p)
-# Units: [rad]
-# [1] 0.541   NaN
-# R: p = st_sfc(st_point(c(7,52)), st_point(c(8,53)), st_point(c(8,54)), crs = 4326)
-# R: st_geod_azimuth(p)
-# Units: [rad]
-# [1] 0.541 0.000
