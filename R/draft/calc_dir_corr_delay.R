337452612bd861d801e2eda43d435f4675e1cfd7
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-17 13:36:30 -0300

    fix missing data.table::

--- R/draft/calc_dir_corr_delay.R
+++ R/draft/calc_dir_corr_delay.R
@@ -8,8 +8,10 @@ calc_dir_corr_delay <- function(DT, edges, window) {
     ID1 = first(ID1),
     ID2 = first(ID2)
     ), by = fusionID]
-  id_tg[, min_tg := fifelse(tg - window < min(tg), min(tg), tg - window), by = fusionID]
-  id_tg[, max_tg := fifelse(tg + window < min(tg), min(tg), tg + window), by = fusionID]
+  id_tg[, min_tg := data.table::fifelse(tg - window < min(tg), min(tg), tg - window),
+        by = fusionID]
+  id_tg[, max_tg := data.table::fifelse(tg + window < min(tg), min(tg), tg + window),
+        by = fusionID]
 
   id_tg[, delay_tg := {
     focal_az <- DT[timegroup == .BY$tg & id == ID1, az]
@@ -19,11 +21,11 @@ calc_dir_corr_delay <- function(DT, edges, window) {
 
   id_tg[, dir_corr_delay := tg - delay_tg]
 
-  setnames(id_tg,  c('tg'), c('timegroup'))
-  set(id_tg, j = c('min_tg', 'max_tg','delay_tg'), value = NULL)
-  setorder(id_tg, timegroup, ID1, ID2, dir_corr_delay)
+  data.table::setnames(id_tg,  c('tg'), c('timegroup'))
+  data.table::set(id_tg, j = c('min_tg', 'max_tg','delay_tg'), value = NULL)
+  data.table::setorder(id_tg, timegroup, ID1, ID2, dir_corr_delay)
 
-  rbindlist(list(
+  out <- data.table::rbindlist(list(
     id_tg,
     id_tg[, .(timegroup,  dyadID, fusionID,
               ID1 = ID2, ID2 = ID1, dir_corr_delay = - dir_corr_delay)]
