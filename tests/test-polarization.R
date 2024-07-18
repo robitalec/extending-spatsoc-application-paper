4f30a22e56bd80e73b105c8f7447cedd55905b8f
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-18 14:36:56 -0300

    fix coord proj/longlat mix

--- tests/test-polarization.R
+++ tests/test-polarization.R
@@ -54,7 +54,7 @@ DT_fogo[, datetime := as.POSIXct(datetime, tz = 'UTC')]
 group_times(DT_fogo, datetime = 'datetime', threshold = '20 minutes')
 group_pts(DT_fogo, threshold = threshold, id = 'id',
           coords = c('x_proj', 'y_proj'), timegroup = 'timegroup')
-calc_az_sequential(DT_fogo, id = id, coords = coords, projection = 4326)
+calc_az_sequential(DT_fogo, id = id, coords = c('x_long', 'y_lat'), projection = 4326)
 
 calc_polarization(DT_fogo)
 
