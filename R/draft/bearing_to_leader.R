c1f1d4edc9fba2c9f118f0161f33ef583bd17ef9
Alec L. Robitaille <robit.alec@gmail.com>
2024-07-19 11:01:24 -0300

    fix expected colname

--- R/draft/bearing_to_leader.R
+++ R/draft/bearing_to_leader.R
@@ -12,19 +12,19 @@ bearing_to_leader <- function(DT, coords = c('x', 'y'), group = 'group') {
   stopifnot(first(coords) %in% colnames(DT))
   stopifnot(last(coords) %in% colnames(DT))
   stopifnot(group %in% colnames(DT))
-  stopifnot('rank_dist_group_bearing' %in% colnames(DT))
+  stopifnot('rank_dist_along_group_bearing' %in% colnames(DT))
 
-  check_has_leader <- DT[, .(has_leader = any(rank_dist_group_bearing == 1)),
+  check_has_leader <- DT[, .(has_leader = any(rank_dist_along_group_bearing == 1)),
                          by = c(group)][!(has_leader)]
 
   if (check_has_leader[, .N > 0]) {
-    warning('groups found missing leader (rank_dist_group_bearing == 1): \n',
+    warning('groups found missing leader (rank_dist_along_group_bearing == 1): \n',
             check_has_leader[, paste(group, collapse = ', ')])
   }
 
-  DT[, temp_leader_xcol := .SD[which(rank_dist_group_bearing == 1)],
+  DT[, temp_leader_xcol := .SD[which(rank_dist_along_group_bearing == 1)],
      .SDcols = first(coords), by = c(group)]
-  DT[, temp_leader_ycol := .SD[which(rank_dist_group_bearing == 1)],
+  DT[, temp_leader_ycol := .SD[which(rank_dist_along_group_bearing == 1)],
      .SDcols = last(coords), by = c(group)]
 
   DT[!group %in% check_has_leader$group, dir_to_leader := fifelse(
