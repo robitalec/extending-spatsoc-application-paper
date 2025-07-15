63631434e70fdf038e585beead96d3649552a986
Alec L. Robitaille <robit.alec@gmail.com>
2025-07-15 15:12:17 -0300

    fix simpler plot, rm int/slope calc, rm extra figs comparing n group size

--- tests/test-leader-group-direction.R
+++ tests/test-leader-group-direction.R
@@ -54,76 +54,25 @@ print(DT[group == DT[, .N, group][N > 3, sample(group, 1)],
            group_direction, position_group_direction, rank_position_group_direction)])
 
 
+
 # Plot --------------------------------------------------------------------
-slope <- DT_test[1, tan(group_mean_bearing)]
-intercept <- DT_test[1, group_mean_y - slope * group_mean_x]
-intercept_inv <-  DT_test[1, group_mean_y - (-1/slope) * group_mean_x]
-g <- ggplot(DT_test, aes(x, y, color = id)) +
-  geom_abline(slope = slope, intercept = intercept) +
-  geom_abline(slope = -1/slope, intercept = intercept_inv, linewidth = 0.3) +
-  geom_point() +
-  geom_text(aes(label = rank_dist_along_group_bearing), nudge_y = 0.5) +
-  geom_point(color = 'black', aes(group_mean_x, group_mean_y)) +
-  theme_bw() +
-  lims(x = c(-10, 10), y = c(-10, 10)) +
-  coord_fixed() +
-  guides(color = 'none')
+DT[, N_by_group := .N, group]
 
-print(g)
+sub_DT <- DT[group == DT[N_by_group > 6, sample(group, 1)]]
 
-
-sel_group <- DT_fogo[N_by_group > 6, sample(group, 1)]
-sub_fogo <- DT_fogo[group == sel_group]
-slope_fogo <- sub_fogo[1, tan(group_mean_bearing)]
-intercept_fogo <- sub_fogo[1, group_mean_y_proj - slope_fogo * group_mean_x_proj]
-intercept_inv_fogo <- sub_fogo[1, group_mean_y_proj - (-1/slope_fogo * group_mean_x_proj)]
-
-g_fogo <- ggplot(sub_fogo, aes(x_proj, y_proj, color = id)) +
-  geom_point(size = 0.8) +
-  geom_text(aes(label = rank_dist_along_group_bearing), nudge_y = 2) +
-  geom_point(color = 'black', aes(group_mean_x_proj, group_mean_y_proj)) +
-  geom_abline(slope = slope_fogo, intercept = intercept_fogo) +
-  geom_abline(slope = -1/slope_fogo, intercept = intercept_inv_fogo,
-              linewidth = 0.2) +
+g_DT <- ggplot(sub_DT, aes(X, Y, color = ID)) +
+  geom_spoke(aes(x = centroid_X, y = centroid_Y, angle = drop_units(group_direction),
+                 radius = max(position_group_direction)), color = 'grey30',
+             arrow = arrow()) +
+  geom_spoke(aes(x = centroid_X, y = centroid_Y, angle = drop_units(group_direction),
+                 radius = min(position_group_direction)), color = 'grey30') +
+  geom_point(color = 'black', aes(centroid_X, centroid_Y)) +
+  geom_point(size = 2) +
+  geom_text(aes(label = rank_position_group_direction), nudge_y = 2, size = 6) +
   theme_bw() +
   labs(x = '', y = '') +
   theme(axis.text = element_blank(), axis.ticks = element_blank()) +
   coord_fixed() +
   guides(color = 'none') +
   scale_x_continuous(expand = expansion(add = 10))
-
-
-min_group_sizes <- c(2, 3, 4)
-DT_time_leading <- rbindlist(lapply(min_group_sizes, function(min_size) {
-  DT_fogo[N_by_group > min_size,
-          .(min_group_size = min_size,
-            time_spent_leading = sum(rank_dist_along_group_bearing == 1) / .N),
-          by = id]
-}))
-
-g_fogo_t_lead <- ggplot(DT_time_leading) +
-  geom_point(aes(time_spent_leading, id, color = factor(min_group_size))) +
-  labs(x = 'Time spent leading group', y = '',
-       color = 'Min group size') +
-  xlim(0, 1) +
-  theme_bw() +
-  theme(axis.text.y = element_blank(), axis.ticks.y = element_blank())
-
-min_group_sizes <- c(2, 3, 4)
-DT_mean_rank <- rbindlist(lapply(min_group_sizes, function(min_size) {
-  DT_fogo[N_by_group > min_size,
-          .(min_group_size = min_size,
-            mean_rank = mean(rank_dist_along_group_bearing)),
-          by = id]
-}))
-
-g_fogo_mean_rank <- ggplot(DT_mean_rank) +
-  geom_point(aes(mean_rank, id, color = factor(min_group_size))) +
-  labs(x = 'Mean rank dist along group axis', y = '',
-       color = 'Min group size') +
-  scale_x_reverse(limits = c(10, 1)) +
-  theme_bw() +
-  theme(axis.text.y = element_blank(), axis.ticks.y = element_blank())
-
-
-print(g_fogo + g_fogo_t_lead / g_fogo_mean_rank + plot_layout(guides = 'collect'))
+g_DT
