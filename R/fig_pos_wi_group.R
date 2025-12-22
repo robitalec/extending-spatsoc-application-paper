288c92d8aaefadac3c95d630dc6741f476707c1a
Alec L. Robitaille <robit.alec@gmail.com>
2025-12-22 15:01:45 -0400

    fst fig_pos_wi_group

---
+++ R/fig_pos_wi_group.R
@@ -0,0 +1,11 @@
+fig_pos_wi_group <- function(DT) {
+  g1 <- ggplot(DT) +
+    geom_histogram(aes(direction_centroid), bins = 30) +
+    labs(x = 'Direction to group centroid', y = '')
+
+  g2 <- ggplot(DT) +
+    stat_halfeye(aes(distance_centroid, factor(rank_distance_centroid))) +
+    labs(x = 'Distance to group centroid', y = 'Rank distance to group centroid')
+
+  g1 + g2
+}
