2f7acd7d0f2dd39425c9e3e9f1da0a46fbbd02a5
Alec L. Robitaille <robit.alec@gmail.com>
2025-12-22 14:46:42 -0400

    man

--- R/write_gs4_to_csv.R
+++ R/write_gs4_to_csv.R
@@ -1,6 +1,14 @@
+#' Write gs4 to CSV
+#'
+#' @param ss Google Sheet identifier
+#' @param sheet sheet name
+#' @param file output file path
+#'
+#' @returns output file path after writing out file
+#' @seealso [googlesheet4::read_sheet()]
 write_gs4_to_csv <- function(ss, sheet, file) {
-  in_sheet <- read_sheet(ss, sheet, col_types = 'c')
-  fwrite(x = in_sheet, file = file)
+  in_sheet <- googlesheets4::read_sheet(ss, sheet, col_types = 'c')
+  data.table::fwrite(x = in_sheet, file = file)
 
   return(file)
 }
