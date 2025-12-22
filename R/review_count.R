56c980784e84e48641973324d136594626996718
Alec L. Robitaille <robit.alec@gmail.com>
2025-12-22 14:46:17 -0400

    rename, combine, man

---
+++ R/review_count.R
@@ -0,0 +1,26 @@
+#' Review count
+#'
+#' @param DT input data.table
+#'
+#' @returns counts of review characteristics eg. programming language, metric used
+review_count <- function(DT) {
+  count_list <- function(DT, col) {
+    count <- DT[, strsplit(.SD[[1]], ';'), .SDcols = col, by = covidence_number]
+    count[, (paste0('n_', col)) := trimws(V1)]
+    return(count)
+  }
+
+  count_programming_language <- count_list(DT, 'programming_language')
+  count_software <- count_list(DT, 'software_package_s_used')
+  count_analysis_code_availability <- count_list(DT, 'analysis_code_availability')
+  count_raw_metric <- count_list(DT, 'metric_used_or_described')
+  count_metric <- count_list(DT, 'metric_agg')
+
+  list(
+    count_prog_lang = count_programming_language,
+    count_software = count_software,
+    count_code_avail = count_analysis_code_availability,
+    count_metric = count_metric,
+    count_raw_metric = count_raw_metric
+  )
+}
