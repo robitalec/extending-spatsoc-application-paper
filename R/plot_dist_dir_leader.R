ab316f3444b0c9ad5d5d7c779b621ddda680c48a
Alec L. Robitaille <robit.alec@gmail.com>
2025-12-23 14:41:13 -0400

    fst plot_dist_dir_leader

---
+++ R/plot_dist_dir_leader.R
@@ -0,0 +1,19 @@
+plot_dist_dir_leader <- function(DT) {
+  DT[, N_by_group := .N, group]
+  # sel_group <- DT[N_by_group > 3, sample(group, 1)]
+  sel_group <- 1025
+  sub_DT <- DT[group == sel_group]
+
+  g_dist <- ggplot(DT, aes(direction_leader,
+                           factor(rank_position_group_direction))) +
+    stat_halfeye() +
+    labs(x = 'Direction to leader', y = 'Rank along group direction') +
+    theme_bw()
+
+  g_dir <- ggplot(DT, aes(distance_leader, factor(rank_position_group_direction))) +
+    stat_halfeye() +
+    labs(x = 'Distance to leader', y = 'Rank along group direction') +
+    theme_bw()
+
+  g_dist + g_dir
+}
