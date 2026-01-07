9660ddf3e77e56aebc61799ad7772803e7f1f790
Alec L. Robitaille <robit.alec@gmail.com>
2026-01-07 13:19:48 -0400

    rm backup fig

--- R/plot_dir_polarization.R
+++ R/plot_dir_polarization.R
@@ -2,25 +2,6 @@ plot_dir_polarization <- function(DT) {
   sel_N <- DT[, .N, group][N > 2, group]
   sub <- DT[group %in% sel_N]
 
-  # sub[, round_pol := round(polarization, 1)]
-  # sub_pol <- sub[round_pol %in% seq(0, 1, by = 0.2),
-  #            .SD[group == first(group)], by = round_pol]
-  #
-  # ggplot(sub_pol, aes(direction)) +
-  #   stat_interval(.width = 1, color = 'black', linewidth = 0.5) +
-  #   geom_point(aes(direction, 0)) +
-  #   geom_text(
-  #     x = 0, y = 0.3,
-  #     aes(label = format(polarization, digits = 2))
-  #   ) +
-  #   facet_grid(round(polarization, digits = 2) ~ .,
-  #              as.table = FALSE) +
-  #   theme_bw() +
-  #   theme(axis.text.y = element_blank(),
-  #         axis.ticks.y = element_blank(),
-  #         ) +
-  #   labs(y = NULL)
-
   drop_groups <- DT[, any(direction > as_units(pi - 0.5, 'rad') |
                            direction < as_units(-pi + 0.5, 'rad') |
                             .N < 3),
