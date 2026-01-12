80c493f8215e87d0728c41ebab15be36589d65ef
Alec L. Robitaille <robit.alec@gmail.com>
2026-01-12 12:15:04 -0400

    fix plot pos group dir
    show example group positions, hist of pos along group dir

--- R/plot_pos_group_dir.R
+++ R/plot_pos_group_dir.R
@@ -1,17 +1,17 @@
 plot_pos_group_dir <- function(DT) {
   DT[, N_by_group := .N, group]
-  sel_group <- DT[N_by_group > 3, sample(group, 1)]
-  print(sel_group)
+  # sel_group <- DT[N_by_group > 3, sample(group, 1)]
   sel_group <- 932
   sub_DT <- DT[group == sel_group]
 
-  sub_DT[, c('centroid_X', 'centroid_Y') := data.frame(st_coordinates(centroid))]
+  sub_DT[, c('centroid_X', 'centroid_Y') :=
+           data.frame(st_coordinates(centroid))]
 
   slope <- sub_DT[1, as.numeric(tan(group_direction))]
   intercept <- sub_DT[1, centroid_Y - slope * centroid_X]
   intercept_inv <- sub_DT[1, centroid_Y - (-1/slope * centroid_X)]
 
-  g_pos <- ggplot(sub_DT, aes(x = X, y = Y)) +
+  g_xy <- ggplot(sub_DT, aes(x = X, y = Y)) +
     geom_spoke(
       aes(centroid_X, centroid_Y,
         radius = max(as.numeric(position_group_direction)),
@@ -36,32 +36,19 @@ plot_pos_group_dir <- function(DT) {
     geom_label(aes(
       label = paste(format(position_group_direction, digits = 1), 'm')
     ), ) +
-    theme_bw(base_size = font_size) +
     labs(x = '', y = '') +
     theme(axis.text = element_blank(), axis.ticks = element_blank()) +
     coord_sf() +
     guides(color = 'none') +
     scale_x_continuous(expand = expansion(add = 10))
 
-  g_hist <- ggplot(DT[N_by_group > 1]) +
-    geom_histogram(aes(units::as_units(position_group_direction, 'm')), binwidth = 1) +
-    labs(x = 'Distance along group direction', y = '') +
-    theme_bw(base_size = font_size)
-
-  # g_hist2 <- ggplot(DT[N_by_group > 1]) +
-  #   geom_histogram(aes(rank_position_group_direction), binwidth = 1) +
-  #   labs(x = 'Rank along group az', y = '')
-
-  g_dist <- ggplot(DT[N_by_group > 1]) +
+  g_pos <- ggplot(DT[N_by_group > 1]) +
     stat_halfeye(aes(units::as_units(position_group_direction, 'm'),
                      factor(rank_position_group_direction))) +
-    labs(x = 'Distance along group direction',
+    labs(x = 'Position along group direction',
          y = 'Rank along group direction') +
     scale_y_discrete(limits = rev(levels(factor(DT$rank_position_group_direction))))
 
-  list(
-    positions = g_pos,
-    dist_dir = (g_dist + g_hist) +
-      plot_annotation(tag_levels = tag_levels, tag_suffix = tag_suffix)
-  )
+  (g_xy / g_pos) +
+    plot_annotation(tag_levels = tag_levels, tag_suffix = tag_suffix)
 }
