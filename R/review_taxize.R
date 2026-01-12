914d762b2bf792b5b21287230dcf23abae1d9e74
Alec L. Robitaille <robit.alec@gmail.com>
2026-01-12 13:03:24 -0400

    fix manual species/parse edits

--- R/review_taxize.R
+++ R/review_taxize.R
@@ -54,6 +54,20 @@ review_taxize <- function(DT) {
           DT_out[, uniqueN(canonicalsimple)],
           ' to ',
           DT_out[, uniqueN(fix_parse)])
+
+  # Manually fix species input / parsing output
+  DT_out[covidence_number == 1444, fix_parse := 'Papio ursinus']
+  DT_out[fix_parse == 'Humans NA', fix_parse := 'Homo sapiens']
+  DT_out[grepl('viginianus', fix_parse),
+         fix_parse := gsub('viginianus', 'virginianus', fix_parse)]
+  DT_out[grepl('heifer', species), fix_parse := 'Bos taurus']
+  DT_out[grepl('Holstein|Angus', species), fix_parse := 'Bos taurus']
+  DT_out[grepl('horses', species), fix_parse := 'Equus ferus']
+  DT_out[grepl('geoffroyi', fix_parse), fix_parse :=
+           gsub('geoffroyi', 'geoffroy', fix_parse)]
+  DT_out[grepl('Ciconia ciconi', fix_parse), fix_parse := 'Ciconia ciconia']
+  DT_out[, .N, fix_parse]
+
   setDT(DT_out)
   setnames(DT_out,
            c('canonicalsimple', 'quality'),
