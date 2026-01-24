0bd1dccf7e22a2609d6ddb2cc8745c621c293de8
Alec L. Robitaille <robit.alec@gmail.com>
2026-01-24 16:34:30 -0330

    fix name, reorg panels

--- R/plot_dist_dir_cent.R
+++ R/plot_dist_dir_cent.R
@@ -1,13 +1,13 @@
 plot_dist_dir_cent <- function(DT) {
-  g1 <- ggplot(DT) +
+  g_dir <- ggplot(DT) +
     geom_histogram(aes(x = direction_centroid), bins = 30) +
     labs(x = 'Direction to group centroid', y = '')
 
-  g2 <- ggplot(DT) +
+  g_dist <- ggplot(DT) +
     stat_halfeye(aes(x = units::as_units(distance_centroid, 'm'),
                      y = factor(rank_distance_centroid))) +
     labs(x = 'Distance to group centroid', y = 'Rank distance to group centroid')
 
-  g1 + g2 +
+  g_dist + g_dir +
     plot_annotation(tag_levels = tag_levels, tag_suffix = tag_suffix)
 }
