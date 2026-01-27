1ae4cf860d69e28fbbca0de67a872c24ec3be49b
Alec L. Robitaille <robit.alec@gmail.com>
2026-01-27 14:09:01 -0330

    fix rev y axis order

--- R/plot_dist_dir_cent.R
+++ R/plot_dist_dir_cent.R
@@ -1,12 +1,13 @@
 plot_dist_dir_cent <- function(DT) {
-  g_dir <- ggplot(DT) +
-    geom_histogram(aes(x = direction_centroid), bins = 30) +
-    labs(x = 'Direction to group centroid', y = '')
-
   g_dist <- ggplot(DT) +
     stat_halfeye(aes(x = units::as_units(distance_centroid, 'm'),
                      y = factor(rank_distance_centroid))) +
-    labs(x = 'Distance to group centroid', y = 'Rank distance to group centroid')
+    labs(x = 'Distance to group centroid', y = 'Rank distance to group centroid') +
+    scale_y_discrete(limits = rev(levels(factor(DT$rank_distance_centroid))))
+
+  g_dir <- ggplot(DT) +
+    geom_histogram(aes(x = direction_centroid), bins = 30) +
+    labs(x = 'Direction to group centroid', y = '')
 
   g_dist + g_dir +
     plot_annotation(tag_levels = tag_levels, tag_suffix = tag_suffix)
