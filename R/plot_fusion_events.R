6b7e6cb588c3a632d38f68fd3bb6d48f256b9111
Alec L. Robitaille <robit.alec@gmail.com>
2026-03-08 11:11:11 -0300

    revert legend within

--- R/plot_fusion_events.R
+++ R/plot_fusion_events.R
@@ -73,9 +73,7 @@ plot_fusion_events <- function(edges, DT) {
     g_tab &
     theme_void(base_size = font_size) &
     theme(
-      plot.background = element_rect(fill = 'white', color = 'white'),
-      legend.position = 'inside',
-      legend.position.inside = c(0.95, 0.2)
+      plot.background = element_rect(fill = 'white', color = 'white')
     )) +
     plot_annotation(tag_levels = tag_levels, tag_suffix = tag_suffix) +
     plot_layout(widths = 1, heights = 1)
